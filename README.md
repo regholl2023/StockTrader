d0b6e6fcb4d23bda0885efd6b7bc6b1d29bec28b
JamesLarson8 <jmlarson8@gmail.com>
2023-10-15 02:29:34 -0400

    updated app.py

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # StockTrader
 A personal stock trading platform that uses Alpaca API.
+
+
+Activate environment by navigating to stocktradingproj directory and enter:
+source venv/bin/activate
+
--- app.py
+++ app.py
@@ -1,24 +1,37 @@
 from flask import Flask, jsonify
-from alpaca_trade_api import REST
+import alpaca_trade_api
 from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
-load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 #API Key
 API_KEY_ID = os.environ.get('ALPACA_API_KEY_ID')
 API_KEY_SECRET = os.environ.get('ALPACA_API_KEY_SECRET')
 
 # Init Alpaca API 
-api = REST('ALPACA_API_KEY_ID', 'ALPACA_API_KEY_SECRET', base_url='https://paper-api.alpaca.markets')  # Use paper trading endpoint for testing
+# api = alpaca_trade_api.REST('ALPACA_API_KEY_ID', 'ALPACA_API_KEY_SECRET', base_url='https://paper-api.alpaca.markets')  # Use paper trading endpoint for testing
+api = alpaca_trade_api.REST('PKQT7VYXEZ5F540Q171B', 'pIxVNiOVpkxyTvU3hZTF5blucCRLxCILMSr9eI4q', base_url='https://paper-api.alpaca.markets')  # Use paper trading endpoint for testing
 
 @app.route('/')
 def index():
-    return "Welcome to the Automated Stock Trading Platform!"
+    # Get account information
+    account_data = api.get_account()._raw
+    # Get cash balance
+    cash_balance = float(account_data['cash'])
+    
+    # Create a welcome message
+    welcome_message = "Welcome to the Automated Stock Trading Platform!"
+    
+    # Combine the welcome message and cash balance as HTML
+    page_content = f"<h1>{welcome_message}</h1><p>Cash Balance: ${cash_balance:.2f}</p>"
+    return page_content
 
-@app.route('/account')
-def account_info():
-    return jsonify(api.get_account()._raw)
+# @app.route('/account')
+# def account_info():
+#     return jsonify(api.get_account()._raw)
 
 if __name__ == '__main__':
     app.run(debug=True)
